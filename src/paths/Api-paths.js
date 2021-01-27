0ad33756475bcbf76cc5349f93062879ecb598c2
idsintehittapa <jblom15@gmail.com>
2021-01-27 17:15:12 +0100

    updated paths

--- src/paths/Api-paths.js
+++ src/paths/Api-paths.js
@@ -3,6 +3,6 @@ export const sendShare = 'http://localhost:8080/testimonies'
 // reuse this?
 export const importTestimonies = 'http://localhost:8080/testimonies'
 
-export const signUp = 'http://localhost:8080/signup'
-export const logIn = 'http://localhost:8080/login'
+export const signUp = 'http://localhost:8080/users'
+export const logIn = 'http://localhost:8080/sessions'
 
