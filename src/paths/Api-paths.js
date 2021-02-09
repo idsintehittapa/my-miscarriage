97bf6ace523a95d4724468e2aa55e5b8c09b7278
idsintehittapa <jblom15@gmail.com>
2021-02-09 17:28:37 +0100

    refactoring code

--- src/paths/Api-paths.js
+++ src/paths/Api-paths.js
@@ -10,3 +10,8 @@ export const moderatorTestimoniesId = 'https://mymiscarriage.herokuapp.com/moder
 // export const signUp = 'https://mymiscarriage.herokuapp.com/users'
 // export const signIn = 'https://mymiscarriage.herokuapp.com/sessions'
 // export const moderatorTestimonies = 'http://localhost:8080/moderator/pending/'
+
+// export const importTestimonies = 'https://mymiscarriage.herokuapp.com/testimonies'
+// export const signUp = 'https://mymiscarriage.herokuapp.com/users'
+// export const signIn = 'https://mymiscarriage.herokuapp.com/sessions'
+// export const moderatorTestimonies = 'http://localhost:8080/moderator/pending/'
